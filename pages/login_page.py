90047c48e6185a612453bc2be25c02aa124aae77
VelagalaSai <Velagala.SaiBhaskari@cognizant.com>
2026-06-11 20:36:48 +0530

    Create login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,48 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    # URL
+    URL = "https://www.saucedemo.com"
+
+    # Locators
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def open(self):
+        self.driver.get(self.URL)
+
+    def enter_username(self, username):
+        field = self.wait.until(EC.presence_of_element_located(self.USERNAME_INPUT))
+        field.clear()
+        field.send_keys(username)
+
+    def enter_password(self, password):
+        field = self.wait.until(EC.presence_of_element_located(self.PASSWORD_INPUT))
+        field.clear()
+        field.send_keys(password)
+
+    def click_login(self):
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_BUTTON)).click()
+
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
+
+    def get_error_message(self):
+        try:
+            error = self.wait.until(EC.presence_of_element_located(self.ERROR_MESSAGE))
+            return error.text
+        except:
+            return ""
+
+    def is_login_successful(s
